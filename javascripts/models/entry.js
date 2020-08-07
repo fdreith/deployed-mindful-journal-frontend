cdd0f679ac9865e5c9994721f3bec9febba19d4b
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-07 14:43:47 -0700

    fixing safari error

--- javascripts/models/entry.js
+++ javascripts/models/entry.js
@@ -1,18 +1,17 @@
 class Entry {
-  static all = []
-
   constructor(data) {
-    this.id = data.id
-    this.prompt = this.findPrompt(data)
-    this.mood = this.findMood(data)
-    this.minutes = data.minutes
-    this.content = data.content
-    this.created_at = new Date(data.created_at)
-    this.save()
+    this.id = data.id;
+    this.prompt = this.findPrompt(data);
+    this.mood = this.findMood(data);
+    this.minutes = data.minutes;
+    this.content = data.content;
+    this.created_at = new Date(data.created_at);
+    this.save();
   }
+  static all = [];
 
   save() {
-    Entry.all.push(this)
+    Entry.all.push(this);
   }
 
   renderEntry() {
@@ -33,15 +32,14 @@ class Entry {
         <p class="hide">${this.content}</p>
       </div>
     </div>
-    `
+    `;
   }
 
   findPrompt(data) {
-    return Prompt.all.find(prompt => prompt.id === data.prompt_id)
+    return Prompt.all.find((prompt) => prompt.id === data.prompt_id);
   }
 
   findMood(data) {
-    return this.findPrompt(data).mood
+    return this.findPrompt(data).mood;
   }
-
 }
